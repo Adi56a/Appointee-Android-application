adf4c1586cdbb7cd7ae9ab7ef3907234a3aa81b6
Aditya <adityajogdand15@gmail.com>
2026-05-04 18:26:06 +0530

    doctor router case issue

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const doctorRoutes = require('./routes/doctorRoutes');
+const doctorRoutes = require('./routes/DoctorRoutes');
 const mrRoutes = require('./routes/MedicalRepresentativeRoute')
 
 dotenv.config();
